aa5186788ee2bca79127c186f8d7198b0cafe4d5
Jason Jaramillo <jason.araque@hotmail.com>
2023-04-09 12:47:22 +0100

    update polocy, banner

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,17 +1,17 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 53.71369564965506, lng: -6.349364168048878 };
-    // The map, centered at Uluru
+    // The location of Kao Thai food truck
+    const kaothai = { lat: 53.71369564965506, lng: -6.349364168048878 };
+    // The map, centered at Kao Thai food truck
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
+        zoom: 10,
+        center: kaothai,
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
+    // The marker, positioned at Kao Thai food truck
+    /*const marker = new google.maps.Marker({
+        position: kaothai,
         map: map,
-    });
+    });*/
 }
 
 window.initMap = initMap;
